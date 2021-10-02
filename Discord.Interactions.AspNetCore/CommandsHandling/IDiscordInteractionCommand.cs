9c19de4e026734e8a25418b9032eeddcf8dc560f
TehGM <wojop95@gmail.com>
2021-10-03 00:54:32 +0100

    Added marker attributes and interfaces for interaction commands

---
+++ Discord.Interactions.AspNetCore/CommandsHandling/IDiscordInteractionCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace TehGM.Discord.Interactions.CommandsHandling
+{
+    /// <summary>Represents an instance of a class that can be invoked by a Discord Interaction.</summary>
+    public interface IDiscordInteractionCommand
+    {
+        /// <summary>Invokes the command.</summary>
+        /// <param name="message">Received interaction.</param>
+        /// <param name="request">HTTP Request of the interaction.</param>
+        /// <param name="services">Service provider that can be used to retrieve DI services.</param>
+        /// <param name="cancellationToken">Cancellation token used to cancel operations.</param>
+        /// <returns>Response to the interaction.</returns>
+        Task<DiscordInteractionResponse> InvokeAsync(DiscordInteraction message, HttpRequest request, IServiceProvider services, CancellationToken cancellationToken);
+    }
+}
---
+++ Discord.Interactions.AspNetCore/CommandsHandling/IDiscordInteractionCommandCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TehGM.Discord.Interactions.CommandsHandling
+{
+    /// <summary>Represents a class that can create a complex Discord Application Command.</summary>
+    /// <remarks>This interface is meant to be used for Interaction Commands that require more complex setup than attributes provide.</remarks>
+    public interface IDiscordInteractionCommandCreator
+    {
+        /// <summary>Builds an interaction command.</summary>
+        /// <param name="services">Service provider that can be used to retrieve services required for command building.</param>
+        /// <returns>The built interaction command.</returns>
+        DiscordApplicationCommand Build(IServiceProvider services);
+    }
+}
