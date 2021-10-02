1cdb9c3d8960670e170e7f4411fb7ba27d1e74e2
TehGM <wojop95@gmail.com>
2021-10-02 18:01:32 +0100

    Split up response and response data to separate files

--- Discord.Interactions.AspNetCore/Entities/InteractionResponse/DiscordInteractionResponse.cs
+++ Discord.Interactions.AspNetCore/Entities/InteractionResponse/DiscordInteractionResponse.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json;
-using TehGM.Discord.Serialization;
+﻿using Newtonsoft.Json;
 
 namespace TehGM.Discord.Interactions
 {
@@ -17,22 +15,5 @@ namespace TehGM.Discord.Interactions
         {
             this.Type = type;
         }
-    }
-
-    public class DiscordInteractionResponseData
-    {
-        [JsonProperty("tts", NullValueHandling = NullValueHandling.Ignore)]
-        public bool IsTTS { get; set; }
-        [JsonProperty("content", NullValueHandling = NullValueHandling.Ignore)]
-        public string Content { get; set; }
-        [JsonProperty("embeds", NullValueHandling = NullValueHandling.Ignore)]
-        public ICollection<DiscordEmbed> Embeds { get; set; }
-        [JsonProperty("allowed_mentions", NullValueHandling = NullValueHandling.Ignore)]
-        [JsonConverter(typeof(DiscordAllowedMentionsConverter))]
-        public DiscordAllowedMentions AllowedMentions { get; set; }
-        [JsonProperty("flags", NullValueHandling = NullValueHandling.Ignore)]
-        public DiscordInteractionResponseFlags? Flags { get; set; }
-
-        // TODO: add components (sheesh)
     }
 }
---
+++ Discord.Interactions.AspNetCore/Entities/InteractionResponse/DiscordInteractionResponseData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using TehGM.Discord.Serialization;
+
+namespace TehGM.Discord.Interactions
+{
+    public class DiscordInteractionResponseData
+    {
+        [JsonProperty("tts", NullValueHandling = NullValueHandling.Ignore)]
+        public bool IsTTS { get; set; }
+        [JsonProperty("content", NullValueHandling = NullValueHandling.Ignore)]
+        public string Content { get; set; }
+        [JsonProperty("embeds", NullValueHandling = NullValueHandling.Ignore)]
+        public ICollection<DiscordEmbed> Embeds { get; set; }
+        [JsonProperty("allowed_mentions", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonConverter(typeof(DiscordAllowedMentionsConverter))]
+        public DiscordAllowedMentions AllowedMentions { get; set; }
+        [JsonProperty("flags", NullValueHandling = NullValueHandling.Ignore)]
+        public DiscordInteractionResponseFlags? Flags { get; set; }
+
+        // TODO: add components (sheesh)
+    }
+}
